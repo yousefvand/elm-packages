4d9445f18bfe17d2ae1194890d4cb578144c34d8
Remisa Yousefvand <remisa.yousefvand@gmail.com>
2019-03-16 00:26:59 +0330

    beta release

---
+++ index.js
@@ -0,0 +1,9 @@
+const info = require('./lib/info')
+const packages = require('./lib/packages')
+const versions = require('./lib/versions')
+
+module.exports = {
+  info: info,
+  packages: packages,
+  versions: versions
+}
